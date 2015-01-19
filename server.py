0e81490b0dd70d89a36e260149375c2276d77533
xhs <chef@dark.kitchen>
2015-01-19 17:44:39 +0800

    support nginx proxy

--- server.py
+++ server.py
@@ -4,16 +4,12 @@ from bottle import Bottle, static_file, run
 
 app = Bottle()
 
-@app.route('/media')
-def media():
-  return static_file('getusermedia.html', root='./')
-
-@app.route('/peer')
+@app.route('/lab/videochat')
 def peer():
   return static_file('rtcpeerconnection.html', root='./')
 
-@app.route('/vendor/<filepath:path>')
+@app.route('/lab/vendor/<filepath:path>')
 def serve_static(filepath):
   return static_file(filepath, root='./vendor')
 
-run(app, host='192.168.1.100', port=60080)
+run(app, host='localhost', port=60081)
