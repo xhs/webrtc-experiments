c24841cc8f45783c47c7752e6ff134a67bc30091
xhs <chef@dark.kitchen>
2015-01-31 21:40:10 +0800

    add sctp thread, not working yet(returns ABORT)

--- dtls/test_sctp_dtls_ice.c
+++ dtls/test_sctp_dtls_ice.c
@@ -40,6 +40,8 @@ struct sctp_transport {
   struct socket *sk;
   BIO *incoming_bio;
   BIO *outgoing_bio;
+  int incoming_stub;
+  int outgoing_stub;
   GMutex sctp_mutex;
 };
 
@@ -170,21 +172,30 @@ data_received_cb(NiceAgent *agent, guint stream_id, guint component_id,
 {
   struct peer_connection *pc = (struct peer_connection *)user_data;
   struct dtls_transport *dtls = pc->dtls;
+  struct sctp_transport *sctp = pc->sctp;
+
   if (candidate_negotiation_done) {
-    g_mutex_lock(&dtls->dtls_mutex);
     fprintf(stderr, "[ice] received encrypted data of length %u from wire\n", len);
+    g_mutex_lock(&dtls->dtls_mutex);
     BIO_write(dtls->incoming_bio, buf, len);
+    g_mutex_unlock(&dtls->dtls_mutex);
 
     if (SSL_is_init_finished(dtls->ssl) != 1) {
       fprintf(stderr, "continue handshake\n");
+      g_mutex_lock(&dtls->dtls_mutex);
       SSL_do_handshake(dtls->ssl);
+      g_mutex_unlock(&dtls->dtls_mutex);
     } else {
       unsigned char buf[BUFFER_SIZE];
       int nbytes = SSL_read(dtls->ssl, buf, sizeof buf);
-      if (nbytes > 0)
+      if (nbytes > 0) {
         print_binary(buf, nbytes);
+        g_mutex_lock(&sctp->sctp_mutex);
+        BIO_write(sctp->incoming_bio, buf, nbytes);
+        g_mutex_unlock(&sctp->sctp_mutex);
+      }
     }
-    g_mutex_unlock(&dtls->dtls_mutex);
+    
   }
 }
 
@@ -200,7 +211,6 @@ ice_thread(gpointer user_data)
     g_error("unable to create agent");
 
   g_object_set(G_OBJECT(agent), "controlling-mode", pc->role, NULL);
-  g_object_set(G_OBJECT(agent), "keepalive-conncheck", TRUE, NULL); // for google
 
   g_signal_connect(G_OBJECT(agent), "candidate-gathering-done",
     G_CALLBACK(candidate_gathering_done_cb), NULL);
@@ -239,7 +249,7 @@ ice_thread(gpointer user_data)
     "s=-\n"
     "t=0 0\n"
     "a=msid-semantic: WMS\n"
-    "m=application 1 DTLS/SCTP 5000\n"
+    "m=application 1 DTLS/SCTP 6000\n"
     "c=IN IP4 0.0.0.0\n");
 
   gchar **lines = g_strsplit(sdp, "\n", 0);
@@ -259,7 +269,7 @@ ice_thread(gpointer user_data)
     strcat(answer, "a=setup:passive\n");
   strcat(answer,
     "a=mid:data\n"
-    "a=sctpmap:5000 webrtc-datachannel 1024\n");
+    "a=sctpmap:6000 webrtc-datachannel 1024\n");
   g_strfreev(lines);
   printf("local offer:\n%s\n", answer);
 
@@ -340,6 +350,71 @@ l_cleanup:
   return NULL;
 }
 
+static gpointer
+sctp_thread(gpointer user_data)
+{
+  struct peer_connection *pc = (struct peer_connection *)user_data;
+  struct dtls_transport *dtls = pc->dtls;
+  struct sctp_transport *sctp = pc->sctp;
+  char buf[BUFFER_SIZE];
+
+  while (!exit_thread) {
+    if (BIO_ctrl_pending(sctp->incoming_bio) <= 0 && BIO_ctrl_pending(sctp->outgoing_bio) <= 0) {
+      g_usleep(5000);
+      continue;
+    }
+    if (BIO_ctrl_pending(sctp->incoming_bio) > 0) {
+      
+      g_mutex_lock(&sctp->sctp_mutex);
+      int nbytes = BIO_read(sctp->incoming_bio, buf, sizeof buf);
+      g_mutex_unlock(&sctp->sctp_mutex);
+
+      fprintf(stderr, "[sctp] got %d bytes data to read\n", nbytes);
+      send(sctp->incoming_stub, buf, nbytes, 0);
+
+      if (nbytes > 0) {
+        usrsctp_conninput(sctp, buf, nbytes, 0);
+      }
+    }
+    if (BIO_ctrl_pending(sctp->outgoing_bio) > 0) {
+      g_mutex_lock(&sctp->sctp_mutex);
+      int nbytes = BIO_read(sctp->outgoing_bio, buf, sizeof buf);
+      g_mutex_unlock(&sctp->sctp_mutex);
+
+      fprintf(stderr, "[sctp] got %d bytes data to write\n", nbytes);
+      send(sctp->outgoing_stub, buf, nbytes, 0);
+
+      if (nbytes > 0) {
+        g_mutex_lock(&dtls->dtls_mutex);
+        SSL_write(dtls->ssl, buf, nbytes);
+        g_mutex_unlock(&dtls->dtls_mutex);
+      }
+    }
+  }
+
+  return NULL;
+}
+
+static int
+sctp_data_ready_cb(void *reg_addr, void *data, size_t len, uint8_t tos, uint8_t set_df)
+{
+  fprintf(stderr, "[sctp] data ready\n");
+  struct sctp_transport *sctp = (struct sctp_transport *)reg_addr;
+  g_mutex_lock(&sctp->sctp_mutex);
+  BIO_write(sctp->outgoing_bio, data, len);
+  g_mutex_unlock(&sctp->sctp_mutex);
+  return 0;
+}
+
+static int
+sctp_data_received_cb(struct socket *sock, union sctp_sockstore addr, void *data,
+                      size_t len, struct sctp_rcvinfo recv_info, int flags, void *user_data)
+{
+  fprintf(stderr, "[sctp] data received:\n");
+  print_binary((unsigned char *)data, (int)len);
+  return 0;
+}
+
 int
 main(int argc, char *argv[])
 {
@@ -350,9 +425,12 @@ main(int argc, char *argv[])
 
   struct peer_connection *pc = (struct peer_connection *)malloc(sizeof *pc);
   memset(pc, 0, sizeof *pc);
-
   struct dtls_transport *dtls = (struct dtls_transport *)malloc(sizeof *dtls);
   memset(dtls, 0, sizeof *dtls);
+  struct sctp_transport *sctp = (struct sctp_transport *)malloc(sizeof *sctp);
+  memset(sctp, 0, sizeof *sctp);
+  pc->dtls = dtls;
+  pc->sctp = sctp;
 
   if (strcmp(argv[1], "client") == 0)
     pc->role = PEER_CLIENT;
@@ -371,10 +449,70 @@ main(int argc, char *argv[])
   if (context == NULL)
     return EXIT_FAILURE;
   pc->ctx = context;
-  printf("fingerprint: %s\n", pc->fingerprint);
-
-  pc->dtls = dtls;
   init_dtls_transport(context, pc);
+
+  usrsctp_init(0, sctp_data_ready_cb, NULL);
+  usrsctp_register_address(sctp);
+  usrsctp_sysctl_set_sctp_ecn_enable(0);
+  struct socket *sock = usrsctp_socket(AF_CONN, SOCK_STREAM, IPPROTO_SCTP,
+                                       sctp_data_received_cb, NULL, 0, sctp);
+  if (sock == NULL) {
+    fprintf(stderr, "sctp socket error\n");
+    return EXIT_FAILURE;
+  }
+  sctp->sk = sock;
+  sctp->incoming_bio = BIO_new(BIO_s_mem());
+  BIO_set_mem_eof_return(sctp->incoming_bio, -1);
+  sctp->outgoing_bio = BIO_new(BIO_s_mem());
+  BIO_set_mem_eof_return(sctp->outgoing_bio, -1);
+
+  int sd;
+  struct sockaddr_in stub_addr;
+  memset(&stub_addr, 0, sizeof stub_addr);
+  inet_pton(AF_INET, "127.0.0.1", &stub_addr.sin_addr);
+  stub_addr.sin_family = AF_INET;
+
+  sd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+  stub_addr.sin_port = htons(10000);
+  bind(sd, (const struct sockaddr *)&stub_addr, sizeof stub_addr);
+  stub_addr.sin_port = htons(20000);
+  connect(sd, (const struct sockaddr *)&stub_addr, sizeof stub_addr);
+  sctp->incoming_stub = sd;
+
+  sd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+  stub_addr.sin_port = htons(20000);
+  bind(sd, (const struct sockaddr *)&stub_addr, sizeof stub_addr);
+  stub_addr.sin_port = htons(10000);
+  connect(sd, (const struct sockaddr *)&stub_addr, sizeof stub_addr);
+  sctp->outgoing_stub = sd;
+
+  struct linger lopt;
+  lopt.l_onoff = 1;
+  lopt.l_linger = 0;
+  // abort when call close()
+  usrsctp_setsockopt(sock, SOL_SOCKET, SO_LINGER, &lopt, sizeof lopt);
+
+  // allow resetting streams
+  struct sctp_assoc_value av;
+  av.assoc_id = SCTP_ALL_ASSOC;
+  av.assoc_value = 1;
+  usrsctp_setsockopt(sock, IPPROTO_SCTP, SCTP_ENABLE_STREAM_RESET, &av, sizeof av);
+
+  uint32_t nodelay = 1;
+  usrsctp_setsockopt(sock, IPPROTO_SCTP, SCTP_NODELAY, &nodelay, sizeof nodelay);
+
+  struct sctp_initmsg init_msg;
+  memset(&init_msg, 0, sizeof init_msg);
+  init_msg.sinit_num_ostreams = 16;
+  init_msg.sinit_max_instreams = 2048;
+  usrsctp_setsockopt(sock, IPPROTO_SCTP, SCTP_INITMSG, &init_msg, sizeof init_msg);
+
+  struct sockaddr_conn sconn;
+  memset(&sconn, 0, sizeof sconn);
+  sconn.sconn_family = AF_CONN;
+  sconn.sconn_port = htons(6000);
+  sconn.sconn_addr = (void *)sctp;
+  usrsctp_bind(sock, (struct sockaddr *)&sconn, sizeof sconn);
 
   gloop = g_main_loop_new(NULL, FALSE);
 
@@ -382,11 +520,15 @@ main(int argc, char *argv[])
   candidate_gathering_done = FALSE;
   candidate_negotiation_done = FALSE;
   GThread *gthread_ice = g_thread_new("ice thread", &ice_thread, pc);
+  GThread *gthread_sctp = g_thread_new("sctp thread", &sctp_thread, pc);
   g_main_loop_run(gloop);
   exit_thread = TRUE;
 
   g_thread_join(gthread_ice);
+  g_thread_join(gthread_sctp);
   g_main_loop_unref(gloop);
+  usrsctp_deregister_address(sctp);
+  usrsctp_finish();
 
   return EXIT_SUCCESS;
 }
